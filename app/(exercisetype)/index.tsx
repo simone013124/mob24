e37db5c6f0c523cfa6bcafc6ea2f88d41f9713c8
Simone <151656886+simone013124@users.noreply.github.com>
2024-07-02 13:20:33 +0200

    routing categories

--- app/(exercisetype)/index.tsx
+++ app/(exercisetype)/index.tsx
@@ -1,49 +1,95 @@
-import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, FlatList } from 'react-native';
-import { useRouter } from 'expo-router';
+import {
+    StyleSheet,
+    View,
+    Text,
+    Pressable,
+    FlatList,
+    TouchableOpacity,
+    Modal,
+    TouchableWithoutFeedback,
+    Keyboard
+} from 'react-native';
+import {router} from "expo-router";
+import {useState} from "react";
+import {MaterialIcons} from "@expo/vector-icons";
+import Card from '../../components/card';
 
-const exercises = [
-    { id: '1', title: 'beginner' },
-    { id: '2', title: 'intermediate' },
-    { id: '3', title: 'expert' },
-];
 
-export default function ExerciseList() {
-    const router = useRouter();
+export type Review = {
+    title: string,
+    rating: number,
+    body: string,
+    key: string
+}
 
-    const handlePress = (title: string) => {
-        router.push(`/category/${title}`);
+export default function HomePage() {
+    const [modalOpen, setModalOpen] = useState(false);
+    const [reviews, setReviews] = useState<Review[]>([
+        { title: 'Beginner', rating: 5, body: 'lorem ipsum', key: '1' },
+        { title: 'Intermediate', rating: 5, body: 'lorem ipsum', key: '1' },
+        { title: 'Pro', rating: 5, body: 'lorem ipsum', key: '1' },
+
+    ]);
+
+    const addReview = (review:Review) => {
+        review.key = Math.random().toString();
+        setReviews((currentReviews) => {
+            return [review, ...currentReviews];
+        });
+        setModalOpen(false);
     };
 
     return (
-        <View style={styles.container}>
-            <FlatList
-                data={exercises}
-                keyExtractor={(item) => item.id}
-                renderItem={({ item }) => (
-                    <TouchableOpacity onPress={() => handlePress(item.title)}>
-                        <View style={styles.itemContainer}>
-                            <Text style={styles.itemText}>{item.title}</Text>
-                        </View>
-                    </TouchableOpacity>
-                )}
+        <View>
+            <Modal visible={modalOpen} animationType='slide'>
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                    <View style={styles.modalContent}>
+                        <MaterialIcons
+                            name='close'
+                            size={24}
+                            style={{...styles.modalToggle, ...styles.modalClose}}
+                            onPress={() => setModalOpen(false)}
+                        />
+                    </View>
+                </TouchableWithoutFeedback>
+            </Modal>
+
+
+            <MaterialIcons
+                name='add'
+                size={24}
+                style={styles.modalToggle}
+                onPress={() => setModalOpen(true)}
             />
+
+            <FlatList data={reviews} renderItem={({ item }) => (
+                <TouchableOpacity onPress={() => router.push({pathname:item.key, params:item})}>
+                    <Card>
+                        <Text>{ item.title }</Text>
+                    </Card>
+                </TouchableOpacity>
+            )} />
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
+    modalToggle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom: 10,
+        borderWidth: 1,
+        borderColor: '#f2f2f2',
+        padding: 10,
+        borderRadius: 10,
+        alignSelf: 'center',
+    },
+    modalClose: {
+        marginTop: 20,
+        marginBottom: 0,
+    },
+    modalContent: {
         flex: 1,
-        padding: 20,
-        backgroundColor: '#fff',
-    },
-    itemContainer: {
-        padding: 20,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-    },
-    itemText: {
-        fontSize: 18,
-    },
+    }
 });
+
---
+++ components/card.tsx
@@ -0,0 +1,33 @@
+import { StyleSheet, View } from 'react-native';
+import {ReactNode} from "react";
+
+type CardProps = {
+    children?: ReactNode
+}
+export default function Card(props:CardProps){
+    return (
+        <View style={styles.card}>
+            <View style={styles.cardContent}>
+                { props.children }
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+        borderRadius: 6,
+        elevation: 3,
+        backgroundColor: '#fff',
+        shadowOffset: { width: 1, height: 1 },
+        shadowColor: '#333',
+        shadowOpacity: 0.3,
+        shadowRadius: 2,
+        marginHorizontal: 4,
+        marginVertical: 6,
+    },
+    cardContent: {
+        marginHorizontal: 18,
+        marginVertical: 20,
+    }
+});
